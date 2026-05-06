641af84cef60932c3dd5b2ec66b762dd6b5ccd6e
Krishna Kant Maurya <krishnakantmaurya@icloud.com>
2026-05-07 00:00:18 +0530

    aes encryption trying

--- hasher-all/main.go
+++ hasher-all/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
 	"hash/fnv"
+	"io"
 )
 
 const TotalShardDB uint32 = 5
+const SECRET_KEY = "ILOVEYOU"
 
 // we have used here fnv(Fowler-Noll-Vo) non-crypto graphic hash object because of fast hashing which will be easier to scatter the data
 // we don't need here secure hash here so we avoid the "SHA-256"
@@ -29,10 +34,41 @@ func GetSecureShardIndex(shard_id string) string {
 	return hashValue
 }
 
+func GetAESShardIndex(shard_id string) (string, error) {
+	block, _ := aes.NewCipher([]byte(SECRET_KEY))
+
+	aesGCM, _ := cipher.NewGCM(block)
+
+	nonce := make([]byte, aesGCM.NonceSize())
+	_, _ = io.ReadFull(rand.Reader, nonce)
+
+	cipherText := aesGCM.Seal(nonce, nonce, []byte(shard_id), nil)
+	return hex.EncodeToString(cipherText), nil
+}
+
+func GetShardIdFromAES(aes_idx string) (string, error) {
+	data, _ := hex.DecodeString(aes_idx)
+
+	block, _ := aes.NewCipher([]byte(SECRET_KEY))
+
+	aesGCM, _ := cipher.NewGCM(block)
+
+	nonceSize := aesGCM.NonceSize()
+	nonce, cipherText := data[:nonceSize], data[nonceSize:]
+
+	shard_id, _ := aesGCM.Open(nil, nonce, cipherText, nil)
+
+	return string(shard_id), nil
+}
+
 func main() {
 	objectId := "65f1a2b3c4d5e6f7a8b9c0d4"
 	shard := GetShardIndex(objectId)
 	fmt.Printf("The ID %s belongs in Shard: %d\n", objectId, shard)
 	secureShard := GetSecureShardIndex(objectId)
 	fmt.Printf("The ID %s belongs in secure Shard: %s\n", objectId, secureShard)
+	AesShard, _ := GetAESShardIndex(objectId)
+	fmt.Printf("The ID %s belongs in aes Shard: %s\n", objectId, AesShard)
+	shard_id, _ := GetShardIdFromAES(AesShard)
+	fmt.Printf("The ID %s belongs in aes Shard decryptions: %s\n", objectId, shard_id)
 }
